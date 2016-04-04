4ef3fc757c43e911943cfbb0f2a6a3fc59ed268b
Lucas G. Sánchez <unkiwii@gmail.com>
2016-04-04 12:02:14 -0300

    update install scripts

---
+++ install.tmux.sh
@@ -0,0 +1,13 @@
+echo off
+echo + [TMUX] Installing...
+
+$INSTALL tmux
+
+rm ~/.tmux.conf >& /dev/null
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
+
+rm -rf ~/.projects.tmux >& /dev/null
+ln -s $(pwd)/projects.tmux ~/.projects.tmux
+
+echo + [TMUX] Done!
+echo on
