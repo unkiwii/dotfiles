3882c85231b2bd020792d7fc01fb98c7932adc81
Lucas G. Sánchez <unkiwii@gmail.com>
2016-10-21 10:12:19 -0300

    add ldminit.sh to initialize light display manager

---
+++ ldminit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mode="$(xrandr -q|grep -A1 "HDMI1 connected"| tail -1 |awk '{ print $1 }')"
+if [ -n "$mode" ]; then
+	xrandr --output HDMI1 --mode 1920x1080
+	xrandr --output eDP1 --mode 1366x768 --pos 224x1080
+else
+	xrandr --output eDP1 --mode 1366x768
+fi
+
+# set background
+feh --bg-fill '/usr/share/images/bunsen/login/guldan.jpg'
