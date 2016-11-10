c9ffa5d6030179be8564fc822a583d03b2792fde
Lucas G. Sánchez <unkiwii@gmail.com>
2016-11-10 11:22:44 -0300

    changed font size to 10 and reduce border and snap of dwm

--- dwm-config.h
+++ dwm-config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Inconsolata:size=11" };
-static const char dmenufont[]       = "Inconsolata:size=11";
+static const char *fonts[]          = { "Inconsolata:size=10" };
+static const char dmenufont[]       = "Inconsolata:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
