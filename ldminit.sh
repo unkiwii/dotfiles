213355a502b3d984a4915e3de5e8c95a6fa6e328
Lucas G. Sánchez <unkiwii@gmail.com>
2016-11-29 10:21:07 -0300

    update monitor displays

--- ldminit.sh
+++ ldminit.sh
@@ -2,8 +2,7 @@
 
 mode="$(xrandr -q|grep -A1 "HDMI1 connected"| tail -1 |awk '{ print $1 }')"
 if [ -n "$mode" ]; then
-	xrandr --output HDMI1 --mode 1920x1080
-	xrandr --output eDP1 --mode 1366x768 --pos 224x1080
+  xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --output eDP1 --mode 1366x768 --pos 0x736 --rotate normal
 else
 	xrandr --output eDP1 --mode 1366x768
 fi
