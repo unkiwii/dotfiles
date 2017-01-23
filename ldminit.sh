ecb318097676a691d4b19b68ae6e6000ee59c682
Lucas G. Sánchez <unkiwii@gmail.com>
2017-01-23 13:23:41 -0300

    fixed whitespace

--- ldminit.sh
+++ ldminit.sh
@@ -4,5 +4,5 @@ mode="$(xrandr -q|grep -A1 "HDMI1 connected"| tail -1 |awk '{ print $1 }')"
 if [ -n "$mode" ]; then
   xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --output eDP1 --mode 1366x768 --pos 0x736 --rotate normal
 else
-	xrandr --output eDP1 --mode 1366x768
+  xrandr --output eDP1 --mode 1366x768
 fi
