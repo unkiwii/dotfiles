43dde150915e401668aee8334e7563a5b85c60b3
Lucas G. Sánchez <unkiwii@gmail.com>
2024-03-08 16:23:27 -0300

    Update network config

--- install-network.sh
+++ install-network.sh
@@ -14,4 +14,10 @@ iface wlp0s20f3 inet dhcp
         wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
 EOF
 
+cat <<EOF >>/etc/resolv.conf
+nameserver 127.0.0.1
+EOF
+
 echo "vim /etc/network/interfaces and delete duplication of interfaces"
+echo "vim /etc/resolv.conf and move configuration to /etc/bind/named.conf.options"
+echo "There's a named.conf.options file example here"
