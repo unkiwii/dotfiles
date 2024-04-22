00236e1b5e0f81d4ce69ce5841c3b3c1e580fa66
Lucas G. Sánchez <unkiwii@gmail.com>
2024-04-22 10:53:21 -0300

    Add <leader>r to run go files from neovim

--- nvim/ftplugin/go.lua
+++ nvim/ftplugin/go.lua
@@ -4,6 +4,7 @@ vim.bo.shiftwidth = 4
 vim.bo.expandtab = false
 
 vim.keymap.set('n', '<leader>a', ':GoAlt<cr>', { buffer = true })
+vim.keymap.set('n', '<leader>r', ':GoRun<cr>', { buffer = true })
 vim.keymap.set('n', '<leader>gt', ':GoTest<cr>', { buffer = true })
 vim.keymap.set('n', '<leader>gf', ':GoTestFunc<cr>', { buffer = true })
 vim.keymap.set('n', '<leader>k', ':GoDbgKeys<cr>', { buffer = true })
