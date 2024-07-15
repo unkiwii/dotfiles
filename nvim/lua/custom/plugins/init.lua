69b37d25f3bef6d7cb627f6042b3f7b9fb40a0e5
Lucas G. Sánchez <unkiwii@gmail.com>
2024-07-15 11:28:48 -0300

    Add kulala.nvim plugin

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -4,8 +4,7 @@
 -- See the kickstart.nvim README for more information
 return {
 
-  -- Integration with tmux
-  {
+  { -- Integration with tmux
     'preservim/vimux',
     dependencies = {
       'christoomey/vim-tmux-navigator',
@@ -13,6 +12,32 @@ return {
     config = function()
       vim.keymap.set('n', '<leader>vl', ':VimuxRunLastCommand<cr>', { silent = true })
       vim.keymap.set('n', '<leader>vp', ':VimuxPromptCommand<cr>', { silent = true })
+    end,
+  },
+
+  { -- HTTP REST-Client Interface
+    'mistweaverco/kulala.nvim',
+    config = function()
+      local kulala = require 'kulala'
+      kulala.setup {
+        -- default_view, body or headers
+        default_view = 'body',
+        -- dev, test, prod, can be anything
+        -- see: https://learn.microsoft.com/en-us/aspnet/core/test/http-files?view=aspnetcore-8.0#environment-files
+        default_env = 'dev',
+        -- enable/disable debug mode
+        debug = false,
+        -- default formatters for different content types
+        formatters = {
+          json = { 'jq', '.' },
+          xml = { 'xmllint', '--format', '-' },
+          html = { 'xmllint', '--format', '--html', '-' },
+        },
+        -- additional cURL options
+        -- e.g. { "--insecure", "-A", "Mozilla/5.0" }
+        additional_curl_options = {},
+      }
+      vim.keymap.set('n', '<leader>rr', kulala.run, { desc = '[R]un HTTP [R]equest with Kulala' })
     end,
   },
 }
