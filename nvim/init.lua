f16ea8e793e940de771927500a0ce8fecd451720
Lucas G. Sánchez <unkiwii@gmail.com>
2024-07-30 16:49:18 -0300

    Improve gopls

--- nvim/init.lua
+++ nvim/init.lua
@@ -621,7 +621,12 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        gopls = {},
+        gopls = {
+          usePlaceholders = true,
+          staticcheck = true,
+          gofumpt = true,
+        },
+
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
