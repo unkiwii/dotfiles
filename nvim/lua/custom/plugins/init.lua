a45f80d89fcbbc427d27daa53bc788c61545a753
Lucas G. Sánchez <unkiwii@gmail.com>
2025-06-07 22:11:45 -0300

    Remove kulala.nvim from plugins

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -15,32 +15,6 @@ return {
     end,
   },
 
-  { -- HTTP REST-Client Interface
-    'mistweaverco/kulala.nvim',
-    config = function()
-      local kulala = require 'kulala'
-      kulala.setup {
-        -- default_view, body or headers
-        default_view = 'body',
-        -- dev, test, prod, can be anything
-        -- see: https://learn.microsoft.com/en-us/aspnet/core/test/http-files?view=aspnetcore-8.0#environment-files
-        default_env = 'dev',
-        -- enable/disable debug mode
-        debug = false,
-        -- default formatters for different content types
-        formatters = {
-          json = { 'jq', '.' },
-          xml = { 'xmllint', '--format', '-' },
-          html = { 'xmllint', '--format', '--html', '-' },
-        },
-        -- additional cURL options
-        -- e.g. { "--insecure", "-A", "Mozilla/5.0" }
-        additional_curl_options = {},
-      }
-      vim.keymap.set('n', '<leader>rr', kulala.run, { desc = '[R]un HTTP [R]equest with Kulala' })
-    end,
-  },
-
   { -- Integration with delve a Go debugger
     'sebdah/vim-delve',
   },
