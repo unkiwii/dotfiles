de83bea05e51b6af1cf6c27aa34dd5181aaa1405
Lucas G. Sánchez <unkiwii@gmail.com>
2025-08-27 10:16:26 -0300

    Set termguicolors to true

--- nvim/init.lua
+++ nvim/init.lua
@@ -90,6 +90,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
 
+vim.o.termguicolors = true
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
@@ -883,18 +885,21 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'ellisonleao/gruvbox.nvim',
+
     priority = 1000, -- Make sure to load this before all the other start plugins.
     opts = {
+      -- gruvbox options
       contrast = 'hard',
     },
-    init = function()
+    config = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+
       vim.cmd.colorscheme 'gruvbox'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 
