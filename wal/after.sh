11b9165ff04ceb612acb861b6ff10313eb64b5c4
Lucas G. Sánchez <unkiwii@gmail.com>
2025-12-01 11:58:00 -0300

    wallpapers: set only background, do not change colors

--- wal/after.sh
+++ wal/after.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env sh
 
-# update xrdb
-xrdb -merge ~/.Xresources
-xrdb -merge ~/.cache/wal/colors-st.Xresources
-
-# reload all st instances
-pkill --signal USR1 -x st
-
-# reload all nvim instances
-pkill --signal USR1 -x nvim
-
-# TODO:update other applications (dmenu, dwm, slstatus, nvim, etc)
+# NOTE do nothing
